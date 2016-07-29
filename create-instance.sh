c4cf513ba3d0a1db2b5ad92876f07f1bc3f0b192
Ron Kurr <kurr@kurron.org>
2016-07-28 21:09:55 -0400

    Improve documentation

---
+++ create-instance.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#AWS_ACCESS_KEY_ID=
+#AWS_SECRET_ACCESS_KEY=
+export AWS_AMI=ami-5f709f34
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_VPC_ID=vpc-27d3f742
+#AWS_ZONE=a
+export AWS_SUBNET_ID=subnet-c8b2c3bf
+export AWS_SECURITY_GROUP=docker-machine
+export AWS_TAGS=plex
+export AWS_INSTANCE_TYPE=t2.micro
+export AWS_DEVICE_NAME=/dev/sda1
+export AWS_ROOT_SIZE=16
+export AWS_VOLUME_TYPE=gp2
+#AWS_INSTANCE_PROFILE
+export AWS_SSH_USER=ubuntu
+#AWS_SSH_KEYPATH=
+
+CMD="docker-machine create --driver amazonec2 plex"
+echo $CMD
+$CMD
